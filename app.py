7163daabff09cd00f055d1ee0a3b5e8633fa0de6
Abid Malik <malikabid2006@gmail.com>
2024-08-25 18:20:10 +0530

    html template render

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, request, jsonify, render_template, send_file
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World! I am coming...'
+    # return 'Hello, World! I am coming...'
+    return render_template('index.html')
