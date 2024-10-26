a96330cb68304693c8be36154bded8b35cc6cd8f
Abid Malik <malikabid2006@gmail.com>
2024-10-26 18:29:33 +0530

    code formatting

--- Includes/image_generator.py
+++ Includes/image_generator.py
@@ -42,20 +42,20 @@ def generate_images_from_csv(csv_file, language="both"):
     num_images_generated = 0
 
     for index, row in df.iterrows():
-        
+
         if language == "both":
             if len(row) > 1:
                 text_line_1 = row[1].strip()  # First line (Hindi)
-                text_line_1 = text_line_1.replace('\t',' ')
+                text_line_1 = text_line_1.replace("\t", " ")
             else:
                 text_line_1 = ""  # Empty field
-            
-            text_line_2 = row[0].replace('\t',' ').strip()  # Second line (English)
+
+            text_line_2 = row[0].replace("\t", " ").strip()  # Second line (English)
         elif language == "english":
             text_line_1 = ""
-            text_line_2 = row[0].replace('\t',' ').strip()  # Only English text
+            text_line_2 = row[0].replace("\t", " ").strip()  # Only English text
         else:  # Hindi only
-            text_line_1 = row[0].strip().replace('\t',' ')
+            text_line_1 = row[0].strip().replace("\t", " ")
             text_line_2 = ""
 
         # Adjust font size if the length of the string is more than 20 characters
@@ -74,22 +74,32 @@ def generate_images_from_csv(csv_file, language="both"):
             [text_width_2, text_height_2] = english_font.font.getsize(text_line_2)[0]
             text_height_1 = text_height_1 if text_height_1 > 90 else 90
             total_height = text_height_1 + text_height_2
-            position_1 = ((image_width - text_width_1) / 2, (image_height - total_height) / 2)
-            position_2 = ((image_width - text_width_2) / 2, (image_height + total_height) / 2 - text_height_2)
+            position_1 = (
+                (image_width - text_width_1) / 2,
+                (image_height - total_height) / 2,
+            )
+            position_2 = (
+                (image_width - text_width_2) / 2,
+                (image_height + total_height) / 2 - text_height_2,
+            )
         elif language == "english":
             [text_width_2, text_height_2] = english_font.font.getsize(text_line_2)[0]
-            position_2 = ((image_width - text_width_2) / 2, (image_height - text_height_2) / 2)
+            position_2 = (
+                (image_width - text_width_2) / 2,
+                (image_height - text_height_2) / 2,
+            )
         else:  # Hindi only
             [text_width_1, text_height_1] = hindi_font.font.getsize(text_line_1)[0]
-            position_1 = ((image_width - text_width_1) / 2, (image_height - text_height_1) / 2)
+            position_1 = (
+                (image_width - text_width_1) / 2,
+                (image_height - text_height_1) / 2,
+            )
 
-
-         # Draw the texts on the image
+        # Draw the texts on the image
         if language != "english":
             draw.text(position_1, text_line_1, fill="black", font=hindi_font)
         if language != "hindi":
             draw.text(position_2, text_line_2, fill="black", font=english_font)
-
 
         # Save the image
         image_path = os.path.join(output_dir, f"{index+1}.jpg")
--- app.py
+++ app.py
@@ -8,43 +8,64 @@ import shutil
 app = Flask(__name__)
 
 # Ensure the upload folder exists
-UPLOAD_FOLDER = 'uploads'
+UPLOAD_FOLDER = "uploads"
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
-@app.route('/')
+
+@app.route("/")
 def hello_world():
     # return 'Hello, World! I am coming...'
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/submit', methods=['POST'])
+
+@app.route("/submit", methods=["POST"])
 def submit():
-    file = request.files.get('file')
-    language_option = request.form.get('languageOption', 'both')
+    file = request.files.get("file")
+    language_option = request.form.get("languageOption", "both")
 
     # Save the uploaded file
-    if file and file.filename.endswith('.csv'):
+    if file and file.filename.endswith(".csv"):
         timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
         fileName = f"input_{timestamp}.csv"
-        file_path = os.path.join(app.config['UPLOAD_FOLDER'], fileName)
+        file_path = os.path.join(app.config["UPLOAD_FOLDER"], fileName)
         file.save(file_path)
-        
+
         # Generate images from the CSV file
         # output_dir = f'{UPLOAD_FOLDER}/{timestamp}' # for debugging purpose
-        output_dir, num_images_generated = image_generator.generate_images_from_csv(file_path, language=language_option)
-        
-        # Create a download link for the generated images
-        download_link = f'{output_dir}'
-        # download_link = f'{fileName}'
-        return jsonify({"status": "success", "message": "File uploaded successfully", "file": file.filename, "download_link": download_link, "num_images_generated": num_images_generated})
-    
-    else:
-        return jsonify({"status": "error", "message": "Invalid file type. Only CSV files are allowed."}), 400
+        output_dir, num_images_generated = image_generator.generate_images_from_csv(
+            file_path, language=language_option
+        )
 
-@app.route('/submit_textarea', methods=['POST'])
+        # Create a download link for the generated images
+        download_link = f"{output_dir}"
+        # download_link = f'{fileName}'
+        return jsonify(
+            {
+                "status": "success",
+                "message": "File uploaded successfully",
+                "file": file.filename,
+                "download_link": download_link,
+                "num_images_generated": num_images_generated,
+            }
+        )
+
+    else:
+        return (
+            jsonify(
+                {
+                    "status": "error",
+                    "message": "Invalid file type. Only CSV files are allowed.",
+                }
+            ),
+            400,
+        )
+
+
+@app.route("/submit_textarea", methods=["POST"])
 def submit_textarea():
-    csv_data = request.form.get('csvTextarea')
-    language_option = request.form.get('languageOption', 'both')
+    csv_data = request.form.get("csvTextarea")
+    language_option = request.form.get("languageOption", "both")
 
     if not csv_data:
         return jsonify({"status": "error", "message": "No CSV data provided."}), 400
@@ -52,11 +73,11 @@ def submit_textarea():
     try:
         # Create a timestamped filename
         timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-        filename = f'input_{timestamp}.csv'
+        filename = f"input_{timestamp}.csv"
         file_path = os.path.join(UPLOAD_FOLDER, filename)
 
         # Save the CSV data to a file
-        with open(file_path, 'w', newline='') as csv_file:
+        with open(file_path, "w", newline="") as csv_file:
             writer = csv.writer(csv_file)
             reader = csv.reader(csv_data.splitlines())
             for row in reader:
@@ -66,22 +87,33 @@ def submit_textarea():
 
         # Generate images from the CSV file
         # output_dir = f'{UPLOAD_FOLDER}/{timestamp}' # for debugging purpose
-        output_dir, num_images_generated = image_generator.generate_images_from_csv(file_path, language=language_option)
-        
+        output_dir, num_images_generated = image_generator.generate_images_from_csv(
+            file_path, language=language_option
+        )
+
         # Create a download link for the generated images
-        download_link = f'{output_dir}'
+        download_link = f"{output_dir}"
         # download_link = f'{fileName}'
-        return jsonify({"status": "success", "message": "File uploaded successfully", "file": filename, "download_link": download_link, "num_images_generated": num_images_generated})
+        return jsonify(
+            {
+                "status": "success",
+                "message": "File uploaded successfully",
+                "file": filename,
+                "download_link": download_link,
+                "num_images_generated": num_images_generated,
+            }
+        )
     except Exception as e:
         return jsonify({"status": "error", "message": str(e)}), 500
-    
-@app.route('/download/<folder>')
+
+
+@app.route("/download/<folder>")
 def download(folder):
     # Create a zip file of the folder
-    shutil.make_archive(folder, 'zip', folder)
-    
+    shutil.make_archive(folder, "zip", folder)
+
     # Get the path to the zip file
-    zip_file_path = f'{folder}.zip'
-    
+    zip_file_path = f"{folder}.zip"
+
     # Send the zip file for download
     return send_file(zip_file_path, as_attachment=True)
